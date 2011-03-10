4b2b68ba1c8bc5a854396df5ce0c48af2825921d
Michael Knox <mike@sysam.com.au>
2011-03-10 19:50:43 +1100

    Modifictations to support openSUSE

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,18 +10,48 @@
 class apache {
 
     # $basedir is the root dir of the httpd config file tree
-    $basedir = "/etc/httpd"
+    $basedir = $operatingsystem ? {
+        /SuSE/  => "/etc/apache2",
+        default => "/etc/httpd",
+    }
+
+    notice "apache:basedir: $basedir"
+    $vhostdir = $operatingsystem ? {
+        /SuSE/  => "$basedir/conf.d",
+        default => "$basedir/vhosts.d",
+    }
+
+    $docrootdir = $operatingsystem ? {
+        /SuSE/  => "/srv/www/htdocs",
+        default => "/var/www/html",
+    }
 
     # $conffile specifies identity of the global httpd config file
-    $conffile = "$basedir/conf/httpd.conf"
+    $conffile = $operatingsystem ? {
+        /SuSE/   => "$basedir/httpd.conf",
+        default  => "$basedir/conf/httpd.conf",
+    }
 
-    package { "httpd": 
+	$apachesvc = $operatingsystem ? {
+        /SuSE/  => "apache2",
+        default => "httpd",
+	}
+
+    package { "$apachesvc": 
         alias   => "apachePackage",
     } # package
     
     File {
         before => Service["apacheService"],
     } # File
+
+    if $operatingsystem =~ /SuSE/ {
+        file {"$basedir/server-tuning.conf":
+            content => template("apache/server-tuning.conf-$operatingsystem.erb"),
+            require => Package["apachePackage"],
+            notify  => Service["apacheService"],
+        }
+    }
 
     file {
         # determined by $operatingsystem
@@ -30,12 +60,12 @@ class apache {
             require => Package["apachePackage"],
             notify  => Service["apacheService"],
             links   => follow;
-        "/etc/sysconfig/httpd":
-            content => template("apache/sysconfig-httpd.erb"),
+        "/etc/sysconfig/$apachesvc":
+            content => template("apache/sysconfig-$apachesvc.erb"),
             require => Package["apachePackage"],
             notify  => Service["apacheService"];
         # favicon
-        "/var/www/html/favicon.ico":
+        "$docrootdir/favicon.ico":
             source  => "puppet:///modules/apache/favicon.ico",
             require => Package["apachePackage"];
         # where we stash vhosts across all distros
@@ -53,13 +83,19 @@ class apache {
             mode    => "755",
             ensure  => "directory",
             require => Package["apachePackage"];
+        "$basedir/listen.conf":
+            mode    => "755",
+            source  => "puppet:///modules/apache/listen.conf";
     } # file
 
-    service { "httpd":
+    service { "$apachesvc":
         enable     => true,
         ensure     => running,
         hasrestart => true,
-        restart    => "/usr/sbin/apachectl graceful",
+        restart    => $apachesvc ? {
+            /apache2/ => "/usr/sbin/apache2ctl graceful",
+            default   => "/usr/sbin/apachectl graceful",
+        },
         alias      => "apacheService",
         require    => Package["apachePackage"],
     } # service
@@ -111,6 +147,11 @@ class apache {
     #   $ensure  - defaults to 'present'
     #   $content - quoted content or a template
     #   $source  - file to grab with the VirtualHost information
+    #   $vhost   - the hostname to use for this vhost, defacts to $name
+    #   $docroot - docroot to use, defaults to /srv/www/vhosts/$vhostname/htdocs - this needs to be abstracted out as it is a SuSE path
+    #   $cgiroot - cgiroot to use, defaults to /srv/www/vhosts/$vhostname/cgi-bin - this needs to be abstracted out as it is a SuSE path
+    #   $contactEmail - contactEmail to use in vhost config
+    #   $tempplate - template to use, default is "apache/vhost.conf.erb", not used if $content is defined
     #
     # Actions:
     #   sets up a vhost
@@ -125,10 +166,52 @@ class apache {
     #    apache::vhost { "mediawiki":
     #        source => "puppet:///modules/mediawiki/mediawiki.conf",
     #    }
-    define vhost ( $ensure = 'present', $content = '', $source = '') {
-        apache::conf_snippet { "vhost-$name":
+    define vhost ( $ensure = 'present', $content = '', $source = '', $vhost = "",
+        $docroot = "", $cgiroot = "",
+        $contactEmail = '', $template = '' ) {
+        $vhostname = $vhost ? {
+            ''      => "$name",
+            default => "$vhost",
+        }
+
+        $realdocroot = $docroot ? {
+            ''      => "/srv/www/vhosts/$vhostname/htdocs",
+            default => $docroot,
+        }
+        $realcgiroot = $cgiroot ? {
+            ''      => "/srv/www/vhosts/$vhostname/cgi-bin",
+            default => $cgiroot,
+        }
+
+        if ($docroot == '' or $cgiroot == '' ) {
+            file{["/srv/www/vhosts/$vhostname"]:
+                ensure => directory,
+                mode   => 0755,
+            }
+    
+        }
+
+        file{["$realdocroot", "$realcgiroot"]:
+            ensure => directory,
+            mode   => 0755,
+        }
+
+        apache::conf_snippet { "vhost-$vhostname":
             ensure   => $ensure,
-            content  => $content,
+            content  => $content ? {
+                # set content to $content if $content has content
+                ''      => $template ? {
+                    #if $content doesn't have content check $template
+                    '' => $source ? {
+                        # Also check $source, if $source doesn't have content then set $comntent to use the default template
+                        # if $source has content, leave $content as empty
+                        ''      => template("apache/vhost.conf.erb"),
+                        default => '',
+                    },
+                    default => template("$template"),
+                },
+                default => $content,
+            },
             source   => $source,
             location => "vhosts.d",
             filename => $name,
